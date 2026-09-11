b4c5b9abc8755b2873bd271f695a2d0c82254bf9
Marko Cetini <marko.cetini@softgames.de>
2026-09-11 12:17:16 +0200

    Add WebWorker filter implementation and update HTML to include new assets
    - Introduced a new WebWorker filter implementation in `webworkerAll-JfJwoo9z.js` with vertex and fragment shaders for processing.
    - Updated `index.html` to reference the new JavaScript and CSS assets for improved styling and functionality.

--- docs/assets/webworkerAll-JfJwoo9z.js
+++ docs/assets/webworkerAll-JfJwoo9z.js
@@ -1,4 +1,4 @@
-import{G as R,b as S,j as b,M as P,N as I,I as B,a3 as T,T as _,am as O,R as A,w as C,a2 as E,r as k}from"./index-DoKIBxvS.js";import{F as z}from"./Filter-XQERF-LK.js";var U=`in vec2 aPosition;
+import{G as R,b as S,j as b,M as P,N as I,I as B,a3 as T,T as _,am as O,R as A,w as C,a2 as E,r as k}from"./index-MRWSoNyn.js";import{F as z}from"./Filter-onbsL0a3.js";var U=`in vec2 aPosition;
 out vec2 vTextureCoord;
 
 uniform vec4 uInputSize;
