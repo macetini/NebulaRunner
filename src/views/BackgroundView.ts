d7af518e37fe5bb17190f31b404df2a479950573
Marko Cetini <macetini@gmail.com>
2026-09-21 00:03:14 +0200

    refactor(canvas): update asset imports for renderer

--- src/views/BackgroundView.ts
+++ src/views/BackgroundView.ts
@@ -12,7 +12,7 @@ uniform vec4 uOutputTexture;
 vec4 filterVertexPosition() {
     vec2 position = aPosition * uOutputFrame.zw + uOutputFrame.xy;
     position.x = position.x * (2.0 / uOutputTexture.x) - 1.0;
-    position.y = position.y * (2.0 / uOutputTexture.y) - 1.0;
+    position.y = position.y * (2.0 * uOutputTexture.z / uOutputTexture.y) - uOutputTexture.z;
     return vec4(position, 0.0, 1.0);
 }
 
